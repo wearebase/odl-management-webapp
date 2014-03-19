8f468920bc52bdc6f592c6f23e5848612b3decc8
Alejandro Tardín <alejandro.tardin@wds.co>
2014-03-19 11:22:38 +0000

    Sample app with express

---
+++ odl.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello ODL!!');
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
