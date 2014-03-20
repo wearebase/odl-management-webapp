6d5151f0dbb4374951e9747ea3e826e7ab872205
Alejandro Tardín <alejandro.tardin@wds.co>
2014-03-20 10:17:10 +0000

    Port for heroku

--- odl.js
+++ odl.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res){
   res.send('Hello ODL!!');
 });
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
