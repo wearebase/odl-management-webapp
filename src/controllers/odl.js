2155901a3abe368a27a51be16e0d4a3c02f2a611
Alejandro Tardín <alejandro.tardin@wds.co>
2014-03-21 12:05:34 +0000

    Added application configuration module

--- src/controllers/odl.js
+++ src/controllers/odl.js
@@ -1,3 +1,6 @@
+var client = new (require('node-rest-client').Client)();
+var config = require('config').ODL;
+
 module.exports.get = function(req, res) {
-  res.send('Hello ODL!!');
+	res.send(config.greeting);
 }
