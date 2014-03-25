a8001f4c8b5ae546d62af1ad2235ab628b61178f
Alejandro Tardín <alejandro.tardin@wds.co>
2014-03-25 10:18:07 +0000

    Use db url from config if present

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ var config = require('config').DB;
 
 var db = {
 	start: function(cb) {
-		mongoose.connect(util.format('mongodb://%s:%d/%s', config.host, config.port, config.name), cb);		
+		mongoose.connect(config.url || util.format('mongodb://%s:%d/%s', config.host, config.port, config.name), cb);		
 	},
 	stop: function(cb) {
 		mongoose.connection.close(cb);
