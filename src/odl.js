690c08e8936e1926d00efb93536d4d3ef6077fd6
Alejandro Tardín <alejandro@tardin.com>
2014-04-08 22:23:34 +0100

    Moved db to util

--- src/odl.js
+++ src/odl.js
@@ -1,7 +1,7 @@
 var config = require('config');
 var express = require('express');
 var app = express();
-var db = require('./db');
+var db = require('./util/db');
 
 app.use(express.bodyParser());
 
