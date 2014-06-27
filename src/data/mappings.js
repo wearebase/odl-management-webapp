31ba099805062c0833e22837d8f2cb6361c9bdb5
Alejandro Tardín <alejandro@tardin.com>
2014-06-27 22:33:55 +0100

    Added badge for devices checked in

--- src/data/mappings.js
+++ src/data/mappings.js
@@ -5,7 +5,7 @@ module.exports = {
             brand: "Apple",
             name: "iPhone 4S",
             thumbnail: "http://www.clipartbest.com/cliparts/4c9/oqB/4c9oqBKcE.png",
-            image: "http://www.clipartbest.com/cliparts/4c9/oqB/4c9oqBKcE.png"
+            image: "http://media01.versus.io/apple-iphone-5/front/front-1376057249893.flat.jpg"
         }
     },
     "/globalmine-server/v2/deviceAttributes/:id": {
