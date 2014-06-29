75e588ff79843b18237b51d0e75c332b55160599
Alejandro Tardín <alejandro@tardin.com>
2014-06-29 17:37:32 +0100

    Improved mock data

--- src/data/mappings.js
+++ src/data/mappings.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "/globalmine-server/v2/devices/imei/:imei": {
+    "/globalmine-server/v2/devices/imei/0:imei": {
         data: {
             id: "25219",
             brand: "Apple",
@@ -8,8 +8,35 @@ module.exports = {
             image: "http://media01.versus.io/apple-iphone-5/front/front-1376057249893.flat.jpg"
         }
     },
+    "/globalmine-server/v2/devices/imei/1:imei": {
+        data: {
+            id: "25219",
+            brand: "Samsumg",
+            name: "Galaxy S5 ",
+            thumbnail: "http://www.clipartbest.com/cliparts/4c9/oqB/4c9oqBKcE.png",
+            image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVuZSlpNcevyiCjJbTW8sX7gTOKG-Lsg5pvOLMQKq8_JHr42ZPqw"
+        }
+    },
+    "/globalmine-server/v2/devices/imei/2:imei": {
+        data: {
+            id: "25219",
+            brand: "Motorola",
+            name: "Moto G",
+            thumbnail: "http://www.clipartbest.com/cliparts/4c9/oqB/4c9oqBKcE.png",
+            image: "http://www.muycomputer.com/wp-content/uploads/2013/11/motorola_moto_g_front.jpg"
+        }
+    },
+    "/globalmine-server/v2/devices/imei/3:imei": {
+        data: {
+            id: "25219",
+            brand: "Nokia",
+            name: "Lumia 720",
+            thumbnail: "http://www.clipartbest.com/cliparts/4c9/oqB/4c9oqBKcE.png",
+            image: "http://www.esato.com/gfx/phones/gallery/nokia-lumia-720-front.jpg"
+        }
+    },
     "/globalmine-server/v2/deviceAttributes/:id": {
-        data: [ 
+        data: [
             {
                 name: "BrewEnabled",
                 value: "Yes",
